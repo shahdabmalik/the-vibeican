b44eed5ab5cfa482566363ceb4b4873c6222d590
shahdabmalik <shahdab888@gmail.com>
2024-01-29 12:04:49 +0530

    added blog

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7dd223ecf8d442499e22f4b2f4a85649',
+  rootNotionPageId: '3e806461fba0462eb0b8f3be53ed9a44',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
